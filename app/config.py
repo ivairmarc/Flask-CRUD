fa3e050dd28fab9840652f2fa8a2f0064a221609
ivairmarc <ivairwork@outlook.com>
2024-08-13 10:40:37 -0400

    conf

--- app/config.py
+++ app/config.py
@@ -3,5 +3,5 @@ import os
 import configparser
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = (f"mysql+pymysql://root:comspirace@127.0.0.1:3306/crud_flask?charset=utf8")
+    SQLALCHEMY_DATABASE_URI = (f"mysql+pymysql://root:pass@127.0.0.1:3306/crud_flask?charset=utf8")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
